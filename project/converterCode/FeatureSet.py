bf38ec4474854e6a17a9b7a892b9bdd0bba498ef
JH Hylands <jhh521@csteach1.york.ac.uk>
2018-02-11 20:28:56 +0000

    FeatureSet class compiled

--- project/converterCode/FeatureSet.py
+++ project/converterCode/FeatureSet.py
@@ -13,7 +13,6 @@ ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
 #percentile
 
 class FeatureSet():    
-    self.dxNames = ['position','speed','acceleration','jerk'] 
     #differntiate an array
     def dtripdt(x,t):
 	return [(x2-x1)/(t2-t1) for (x1,x2,t1,t2) in zip(x[:-1],x[1:],t[:-1],t[1:])]
@@ -21,6 +20,7 @@ class FeatureSet():
 
     #The class needs to be given the data; which needs to be in J format
     def __init__(self,data):
+    	self.dxNames = ['position','speed','acceleration','jerk'] 
 	self.dOFx = [[],[],[],[]]
 	self.dOFx[1] = data #speeds
 	self.dOFx[2] = [self.dtripdt(trip) for trip in data]
@@ -42,27 +42,27 @@ class FeatureSet():
 
     def min(self,d):
 	self.headings.append('Min ' + self.dxNames[d])
-	self.columns.append([np.min(trip) for trip in self.dOFx[d])
+	self.columns.append([np.min(trip) for trip in self.dOFx[d]])
 
     def max(self,d):
 	self.headings.append('Max ' + self.dxNames[d])
-	self.columns.append([np.max(trip) for trip in self.dOFx[d])
+	self.columns.append([np.max(trip) for trip in self.dOFx[d]])
 
     #standard deviation
     def STD(self,d):
 	self.headings.append('Standard deviation of ' + self.dxNames[d])
-	self.columns.append([np.std(trip) for trip in self.dOFx[d])
+	self.columns.append([np.std(trip) for trip in self.dOFx[d]])
 
     def percentile(self,tiles,d):
 	#quatiles 1,10,25,75,90,99
-	tiles = tiles || [1,10,25,75,90,99] 
+	tiles = tiles or [1,10,25,75,90,99] 
 	#CummulativeFrequenceySpeed
 	CFS = []
 	for tile in tiles:
-		self.columns.append( [np.percentile(trip,tile) for trip in STD])
+		self.columns.append( [np.percentile(trip,tile) for trip in self.dOFx[d]])
 		self.headers.append( str(tile) + ordinal(tile) + ' Percentile of ' + self.dxNames[d])
 
-    def Qrange(self,d)
+    def Qrange(self,d):
 	self.headings.append('Interquatile range of the ' + self.dxNames[d] + 's')
 	Qrange = [np.percentile(trip,75)-np.percentile(trip,25) for trip in self.dOFx[d]]
 	self.columns.append(Qrange)
