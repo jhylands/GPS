955182a913b1997adac06ab10070e706721ef649
JH Hylands <jhh521@york.ac.uk>
2018-04-09 12:14:35 +0100

    crossVal function complete

--- project/crossVal.m
+++ project/crossVal.m
@@ -1,28 +1,15 @@
-function [training,testing] = crossVal(n,x,vector)
-    training  = [vector(1:n*(x-1)); vector(n*x:end)];
-    testing = vector(n*(x-1):n*x);
-end
+function [M,Mv] = crossVal(vector)
+        %ungeneralising
+        ten = [1,2,3,4,5,6,7,8,9,10];
+        ind = ten * size(vector,1)/10
+        M{1} = vector(ind(1):end,:);
+        for x=2:9
+            M{x} = [vector(1:ind(x-1),:); vector(ind(x)+1:end,:)];
+        end 
+        M{10} = vector(1:ind(9)+1,:);
 
-%ungeneralising
-MororisedIndecies = ten * 48
-
-MororisedIndecies =
-
-    48    96   144   192   240   288   336   384   432   480
-
-M{1} = Motorised(49:end,:);                     
-M{2} = [Motorised(1:48,:); Motorised(97:end,:)];
-M{3} = [Motorised(1:96,:); Motorised(145:end,:)];
-M{4} = [Motorised(1:144,:); Motorised(193:end,:)];
-M{5} = [Motorised(1:192,:); Motorised(241:end,:)];
-M{6} = [Motorised(1:240,:); Motorised(289:end,:)];
-M{7} = [Motorised(1:288,:); Motorised(337:end,:)];
-M{8} = [Motorised(1:336,:); Motorised(385:end,:)];
-M{9} = [Motorised(1:384,:); Motorised(433:end,:)];
-M{10} = Motorised(1:432,:);                        
-
-Mv{1} = Motorised(1:48,:);
-for x=2:10                                                           
-    Mv{x} = Motorised(MororisedIndecies(x-1):MororisedIndecies(x),:);
-end                                                                  
- 
+        Mv{1} = vector(1:ind(1),:);
+        for x=2:10                                                           
+            Mv{x} = vector(ind(x-1):ind(x),:);
+        end                                                                  
+end 
