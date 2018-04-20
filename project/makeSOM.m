01c7621eca0b63c911e58c0959328419b60a65cc
JH Hylands <jhh521@york.ac.uk>
2018-04-20 13:56:21 +0100

    update on somMaker

--- project/makeSOM.m
+++ project/makeSOM.m
@@ -26,21 +26,44 @@ training(1,487:540) = 5;
 training(1,541:648) = 1;
 training(1,649:1071) = 3;
 
-%creating and training the network
-for i = 1:5
-    training(2:20,1:486) = Rides';
-    training(2:20,487:540) = Runs';
-    training(2:20,541:648)= Walks';
-    training(2:20,649:1071) = Motorised';
+testing = ones(20,119);
+testing(1,1:54) = 2;
+testing(1,55:60) = 5;
+testing(1,61:72) = 1;
+testing(1,73:119) = 3;
+
+%get cross validation scripts (slow, could do with opermising)
+[I,Iv] = crossVal(Rides);
+[U,Uv] = crossVal(Runs);
+[W,Wv] = crossVal(Walks);
+[M,Mv] = crossVal(Motorised);
+
+%creating and training the network (10 cross validations)
+for i = 1:10
+    
+    training(2:20,1:486) = I{i}; %486 items
+    training(2:20,487:540) = U{i}; %56 items
+    training(2:20,541:648)= W{i}; %108 items
+    training(2:20,649:1071) = M{i}; %423 items
+
+    testing(2:20,1:54) = Iv{i};
+    testing(2:20,55:60) = Uv{i};
+    testing(2:20,61:72) = Wv{i};
+    testing(2:20,73:119) = Mv{i};
+
     net{i} = selforgmap([8 8]);
     net{i} = train(net,training);
+    
+    %images for each of the networks
+    %outputting the planes
+    f = figure;
+    plotsomplanes(net);
+    saveas(f,'img.png');
+    %a results file to be worked with 
 end
+%copy of the networks saved
 
 
-%outputting the planes
-f = figure;
-plotsomplanes(net);
-saveas(f,'img.png');
 
 %plot som hits for each group
 %plotsomhits
